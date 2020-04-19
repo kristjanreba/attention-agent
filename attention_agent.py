595740f16422a9e1cd762883da1eb6c9bf222a64
Kristjan Reba <kristjan.reba96@gmail.com>
2020-04-19 21:55:37 +0200

    Add optimization of the parameters (code looks finihed, but can not test it due to time complexity)

--- attention_agent.py
+++ attention_agent.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
-
-from numpy.lib.stride_tricks import as_strided
+import cma
 
 import torch
 import torch.nn as nn
@@ -36,6 +35,7 @@ def f(k, locations):
 class AttentionAgent():
     
     def __init__(self, L=96, M=7, S=4, K=10):
+        # Attention Agent parameters
         self.L = L # image dimension after resizing is LxL
         self.M = M # patch size
         self.S = S # stride when extracting patches
@@ -44,10 +44,22 @@ class AttentionAgent():
         self.N = ((L-M)//S + 1)**2 # number of patches
         self.d_in = self.M * self.M * 3
 
+        # Learnable parameters
         self.W_k = np.random.random((self.d_in, self.d))
         self.W_q = np.random.random((self.d_in, self.d))
-
         self.controller = nn.LSTM(20, 3, 16)
+
+        # Evolution parameters
+        self.pop_size = 5
+        self.rewards = np.zeros(self.pop_size)
+        self.current_ix = 0
+
+        self.num_parameters = 2 * self.d_in * self.d + np.sum(p.numel() for p in self.controller.parameters())
+        self.es = cma.CMAEvolutionStrategy(self.num_parameters * [0.], 1.)
+        self.parameters = np.array(self.es.ask())
+        self.best_param = None
+        self.best_reward = -np.inf
+        self.vec2param()
 
 
     def act(self, obs):
@@ -67,11 +79,51 @@ class AttentionAgent():
         positions = np.array([f(k, locations) for k in ind]) # index to position mapping
         positions = np.reshape(positions, (1,-1))
 
-        #w = parameters_to_vector(self.controller.parameters()).detach().numpy()
-
         with torch.autograd.no_grad():
             positions = torch.from_numpy(positions.reshape(1, 1, -1))
-            action, _ = self.controller(positions.float())
+            action, _ = self.controller(positions)
         
         action = np.reshape(action.numpy(), (-1,))
         return action
+
+
+    def episode_end(self, reward):
+        if reward > self.best_reward:
+            self.best_param = self.parameters[self.current_ix]
+            self.best_reward = reward
+            np.save('best_param.npy', self.best_param)
+        self.rewards[self.current_ix] = reward
+        self.current_ix += 1
+
+        if self.current_ix >= self.pop_size:
+            self.es.tell(self.parameters, self.rewards)
+            self.parameters = self.es.ask()
+            self.rewards = np.zeros(self.pop_size)
+            self.current_ix = 0
+        else:
+            self.vec2param()
+
+
+    def vec2param(self):
+        """
+        Convert current vector of parameters into matrices W_k, W_q and LSTM controller weights.
+        Input: vector of parameters
+        """
+        pos_ix = 0
+        vec = self.parameters[self.current_ix] # get current vector of params
+        self.W_k = vec[:self.d_in * self.d].reshape((self.d_in,self.d))
+        pos_ix += self.d_in * self.d
+        self.W_q = vec[pos_ix : pos_ix + self.d_in * self.d].reshape((self.d_in,self.d))
+        pos_ix += self.d_in * self.d
+
+        for p in self.controller.parameters():
+            t_shape = p.data.shape
+            prod = np.prod(t_shape)
+            if len(t_shape) == 1: p.data = torch.from_numpy(vec[pos_ix:pos_ix + prod].flatten())
+            else: p.data = torch.from_numpy(vec[pos_ix:pos_ix + prod].reshape(t_shape[0],-1))
+            pos_ix += prod
+
+
+    def test(self):
+        self.parameters = self.best_param
+        self.vec2param()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import gym
-import cma
 from attention_agent import AttentionAgent
 
 
 env_name = 'CarRacing-v0'
 
+env = gym.make(env_name)
+env.close()
 env = gym.make(env_name)
 agent = AttentionAgent()
 
@@ -13,11 +14,34 @@ max_steps = 1000
 
 for i_episode in range(max_episodes):
     observation = env.reset()
+    reward_sum = 0
     for t in range(max_steps):
-        env.render()
+        #env.render()
+
         action = agent.act(observation)
         observation, reward, done, info = env.step(action)
+        reward_sum += reward
         if done:
             print('Episode finished after {} timesteps'.format(t+1))
             break
+    agent.episode_end(reward_sum)
+    print('Episode reward: ', reward_sum)
+
+
+
+# Test
+agent.test()
+observation = env.reset()
+reward_sum = 0
+for t in range(max_steps):
+    env.render()
+    action = agent.act(observation)
+    observation, reward, done, info = env.step(action)
+    reward_sum += reward
+    if done:
+        print('Episode finished after {} timesteps'.format(t+1))
+        break
+print('Episode reward: ', reward_sum)
+
+
 env.close()
